68c4aff866c0de43eb1f39e503a8edaec3841778
Vamsies <167759367+Vamsies@users.noreply.github.com>
2025-06-19 09:59:48 +0530

    Create main.tf

---
+++ terraform/modules/alb/main.tf
@@ -0,0 +1,25 @@
+resource "aws_lb" "app_alb" {
+  name               = "app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = var.security_group_ids
+  subnets            = var.public_subnet_ids
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  port     = 3000
+  protocol = "HTTP"
+  target_type = "instance"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
