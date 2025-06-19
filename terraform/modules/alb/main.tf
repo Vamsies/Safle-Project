bf1b73d07d41dcb0f9478c02ef3893e5ffb2c2c0
Vamsies <167759367+Vamsies@users.noreply.github.com>
2025-06-19 10:24:29 +0530

    Update main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,25 +1,32 @@
-resource "aws_lb" "app_alb" {
-  name               = "app-alb"
-  internal           = false
+resource "aws_lb" "alb" {
+  name               = "${var.app_name}-alb"
   load_balancer_type = "application"
   security_groups    = var.security_group_ids
   subnets            = var.public_subnet_ids
 }
 
-resource "aws_lb_target_group" "app_tg" {
-  port     = 3000
-  protocol = "HTTP"
+resource "aws_lb_target_group" "tg" {
+  name        = "${var.app_name}-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
   target_type = "instance"
-  vpc_id   = var.vpc_id
 }
 
-resource "aws_lb_listener" "app_listener" {
-  load_balancer_arn = aws_lb.app_alb.arn
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
+    target_group_arn = aws_lb_target_group.tg.arn
   }
+}
+
+resource "aws_lb_target_group_attachment" "attach" {
+  count            = length(var.target_ids)
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = var.target_ids[count.index]
+  port             = 3000
 }
