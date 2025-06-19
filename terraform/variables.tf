19d163b4fe2ca00a82b6b131b31f74a433cc6f96
Vamsies <167759367+Vamsies@users.noreply.github.com>
2025-06-19 10:21:29 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,19 @@ variable "app_name" {
 variable "instance_type" {
   default = "t3.micro"
 }
+
+variable "ami_id" {
+  default = "ami-0c02fb55956c7d316"  # Amazon Linux 2 (update as needed)
+}
+
+variable "key_name" {
+  default = "your-key-pair-name"
+}
+
+variable "db_username" {
+  default = "postgres"
+}
+
+variable "db_password" {
+  default = "changeme123"
+}
