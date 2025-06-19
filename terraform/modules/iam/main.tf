a4ef3bfec264d0d19eddf8d77b3ae7f44b3db573
Vamsies <167759367+Vamsies@users.noreply.github.com>
2025-06-19 10:26:30 +0530

    Update main.tf

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,5 +1,23 @@
 resource "aws_iam_role" "ec2_role" {
-  name = "ec2-role"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+  name = "ec2-${var.app_name}-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
 }
-# Add instance profile and policy attachments.
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.app_name}-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
